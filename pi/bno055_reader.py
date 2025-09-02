f85735881eb7523e16e5f660d2be2a7934889cf1
Anandk01 <anand.intern@tranforge.com>
2025-09-03 00:39:56 +0530

    mock bno055

--- pi/bno055_reader.py
+++ pi/bno055_reader.py
@@ -3,9 +3,19 @@ import board, busio
 import adafruit_bno055
 
 class BNO055Reader:
-    def __init__(self, bus=None):
-        i2c = bus or busio.I2C(board.SCL, board.SDA)
-        self.sensor = adafruit_bno055.BNO055_I2C(i2c)
+    def __init__(self, bus=None, mock=False):
+        self.mock = mock
+        if mock:
+            print("BNO055Reader: Running in mock mode")
+            self.sensor = None
+        else:
+            try:
+                i2c = bus or busio.I2C(board.SCL, board.SDA)
+                self.sensor = adafruit_bno055.BNO055_I2C(i2c)
+            except (ValueError, OSError) as e:
+                print(f"BNO055Reader: Sensor not found, switching to mock mode. Error: {e}")
+                self.mock = True
+                self.sensor = None
         self._last_ok = time.time()
 
     @staticmethod
@@ -16,6 +26,8 @@ class BNO055Reader:
         return deg
 
     def yaw_deg(self):
+        if self.mock:
+            return 0.0  # Return fixed yaw for testing
         ypr = self.sensor.euler
         if ypr is None: return None
         yaw = ypr[0]
