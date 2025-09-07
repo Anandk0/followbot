a5c4090c0b25e1217ff893f9c9e33fd97e4bbcbc
Anandk01 <anand.intern@tranforge.com>
2025-09-08 01:00:41 +0530

    eifi creds

--- esp8266_wifi_code.ino
+++ esp8266_wifi_code.ino
@@ -3,8 +3,8 @@
 #include <ArduinoJson.h>
 
 // WiFi credentials
-const char* ssid = "YOUR_WIFI_NAME";
-const char* password = "YOUR_WIFI_PASSWORD";
+const char* ssid = "Poco M6 Pro 52g
+const char* password = "12345679
 
 // Fixed IP configuration
 IPAddress local_IP(10, 163, 221, 143);  // ESP8266 IP in same network as desktop
